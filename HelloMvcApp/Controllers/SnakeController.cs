b918f7179fc899ca2af31b5e493c346b8b7d48d5
Metin Coşkun <klaymanier@gmail.com>
2023-04-03 01:49:41 +0300

    yılan oyunu ekledim

---
+++ HelloMvcApp/Controllers/SnakeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HelloMvcApp.Controllers
+{
+    public class SnakeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View("Index");
+        }
+    }
+}
